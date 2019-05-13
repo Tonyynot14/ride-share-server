2803fce1857a45a9e66f6f9c4b7882068a522885
Tonyynot14 <Wadant3@gmail.com>
2019-05-13 16:06:49 -0400

    Made method to update driver information to start application process

--- admin/app/controllers/api/v1/drivers.rb
+++ admin/app/controllers/api/v1/drivers.rb
@@ -56,6 +56,29 @@ module Api
           return { sucess:true }
         end
       end
+
+      desc "Create Application from App"
+      params do
+        requires :car_make, type: String, desc: " Car Manufactor of Driver"
+        requires :car_model, type: String, desc: " Car Model of Driver"
+        requires :car_year, type: Integer, desc: " Car Year of Driver"
+        requires :car_color, type: String, desc: " Car Color of driver"
+        requires :car_plate, type: String, desc: " Car plate of driver"
+        requires :insurance_provider, type: String, desc: " Insurance Provider for driver"
+        requires :insurance_start, type: DateTime, desc: " Insurance start date"
+        requires :insurance_stop, type: DateTime, desc: " Insurance start date"
+      end
+      post "drivers/application" do
+        driver = current_driver
+          driver.update(car_make: params[:car_make], car_model: params[:car_model],
+                        car_year: params[:car_year], car_color: params[:car_color],
+                        car_plate: params[:car_plate], insurance_provider: params[:insurance_provider],
+                        insurance_start: params[:insurance_start], insurance_stop: params[:insurance_stop],
+                        application_state: "pending")
+
+      end
+
+
     end
   end
 end
